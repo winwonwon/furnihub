7d8c7c65d2d6ec54949da65e8831438b647e310a
achirayaploy <achiraya.mak@student.mahidol.ac.th>
2024-03-29 14:31:00 +0700

    browe update

---
+++ browse.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get elements from the DOM
+    var menubar = document.getElementById("menubar");
+    var navigation = document.getElementById("navigation");
+    var closeIcon = document.getElementById("closeIcon");
+    
+
+    // Toggle nav menu when menu icon is clicked
+    menubar.addEventListener("click", function() {
+        navigation.classList.toggle("active");
+    });
+
+    // Close nav menu when close icon is clicked
+    closeIcon.addEventListener("click", function() {
+        navigation.classList.remove("active");
+    });
+        
+      
+});
