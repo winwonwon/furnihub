96c3e0385770927f6585c348fd322bff3738475f
Pongpon Tangpigulthong <pong@Pongpons-MacBook-Air.local>
2024-04-05 16:13:59 +0700

    project re-structred

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path    = require("path")
+const dotenv  = require("dotenv");
+dotenv.config()
+
+const app = express();
+const router = express.Router();
+app.use("/", router);
+
+app.use("/", express.static(path.join(__dirname, "public")))
+// app.use("/", express.static(path.join(__dirname, "public", "homepage")))
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "homepage", "homepage.html"))
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening to port: ${process.env.PORT}`);
+});
