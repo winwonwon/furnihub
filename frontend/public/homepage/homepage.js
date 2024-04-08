9752bb0e4482b7638bacf289c86fa9f2ac655667
Preeamm <yaowapa.sab@student.mahidol.ac.th>
2024-04-08 11:40:10 +0700

    homepage update

--- frontend/public/homepage/homepage.js
+++ frontend/public/homepage/homepage.js
@@ -21,11 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "../login.html";
     });
 
-    // redirect to products page when register/sign-in link is clicked
+    // redirect to products page when "shop now" is clicked
     document.getElementById("shopNowBtn").addEventListener("click", function() {
         window.location.href = "../products/products.html";
     });
-
+    
     // get all the menu items
     var menuItems = document.querySelectorAll(".navigation ul li a");
 
