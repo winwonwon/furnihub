76251ef7a2b0710e67d129a0799f1c7a66fd1096
Pongpon Tangpigulthong <pong@Pongpons-MacBook-Air.local>
2024-04-21 18:57:14 +0700

    Add routes

--- webservice/api/router.js
+++ webservice/api/router.js
@@ -1,4 +1,6 @@
 const product = require("./product.js")
+const user = require("./users.js")
+const staff = require("./staff.js")
 
 const express = require('express')
 const router = express.Router();
@@ -6,5 +8,7 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.use("/products", product);
+router.use("/users", user);
+router.use("/staff", staff);
 
 module.exports = router
